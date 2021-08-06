48c5e7968fd857032721ef3bd31ae771030c1f96
seanyoung247 <seanyoung247@gmail.com>
2021-08-06 23:33:29 +0100

    feat: Adds basic image handler

--- assets/js/modules/rendermanager.mjs
+++ assets/js/modules/rendermanager.mjs
@@ -1,1 +1,24 @@
 import { Point2D, Vector2D } from "./types2d.mjs";
+
+/**
+ * @module rendermanager
+ */
+/**
+ * Encapsulates a simple image that can be drawn to a canvas
+ */
+class ImgAsset {
+  constructor(image) {
+    this._loaded = false;
+    this._image = new Image();
+    this._image.src = image;
+    this._image.onload = () => this._loaded = true;
+  }
+  get loaded() {return this._loaded;}
+  get pixels() {
+    if (!this._loaded) throw "Image not loaded!";
+    return this._pixels;
+  }
+  get width() {return this._image.width;}
+  get height() {return this._image.height;}
+  get image() {return this._image;}
+}
