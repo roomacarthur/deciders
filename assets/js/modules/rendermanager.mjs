6afbbd0754fab9cf1c76d4bc98a6faff1b189559
seanyoung247 <seanyoung247@gmail.com>
2021-08-06 23:33:53 +0100

    feat: Adds pixel buffer handler

--- assets/js/modules/rendermanager.mjs
+++ assets/js/modules/rendermanager.mjs
@@ -22,3 +22,39 @@ class ImgAsset {
   get height() {return this._image.height;}
   get image() {return this._image;}
 }
+/**
+ * Encapsulates an image that can be read to and from on a per pixel basis
+ */
+class PixelImg {
+  /**
+   * Creates a new Texture
+   *  @param {String} image - The image file path for this texture
+   */
+  constructor(image) {
+    this._loaded = false;
+    this._image = new Image();
+    this._image.src = image;
+    this._image.onload = () => {
+      // Create the buffer for reading the pixel data and move the image into it
+      this._surface = document.createElement('canvas');
+      this._context = this._surface.getContext('2d');
+      this._context.drawImage(this._image, 0, 0);
+      // Create a 32bit pixel buffer for the image pixel data
+      this._data = this._context.getImageData(0,0,this._image.width,this._image.height);
+      this._pixels = new Uint32Array(this._data.data.buffer);
+    });
+      this._loaded = true;
+    }
+  }
+
+  get loaded() {return this._loaded;}
+
+  get pixels() {
+    if (!this._loaded) throw "Image not loaded!";
+    return this._pixels;
+  }
+
+  get width() {return this._image.width;}
+  get height() {return this._image.height;}
+  get image() {return this._image;}
+}
