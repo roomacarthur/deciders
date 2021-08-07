835f96dcf64a94c01db0e715b5277dd871462cf3
seanyoung247 <seanyoung247@gmail.com>
2021-08-07 12:10:28 +0100

    test: Adds sprite test code

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,19 @@
 // Engine Core Test code
 
-import { PixelImg, Renderer } from "./modules/rendermanager.mjs";
+import { ImgAsset, PixelImg, Renderer } from "./modules/rendermanager.mjs";
 
-const floorImg = new PixelImg("./assets/img/tracks/test.png", floorLoaded);
 const renderer = new Renderer(document.getElementById('canvas'));
+const floorImg = new PixelImg("./assets/img/tracks/test.png", floorLoaded);
+const billboardImg = new ImgAsset("./assets/img/sprites/test.png");
 renderer.camera.direction.x = Math.cos(1.57079632679);
 renderer.camera.direction.y = Math.sin(1.57079632679);
+renderer.camera.position.x = 80;
+renderer.camera.position.y = 250;
 
 function floorLoaded() {
   renderer.drawBackdrop();
   renderer.projectFloor(floorImg);
+  renderer.drawSprite(billboardImg, {x:80, y:350});
 }
 
 
