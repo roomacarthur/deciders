10b8407698615c724f9b86fc07c3901eb5e288e0
seanyoung247 <seanyoung247@gmail.com>
2021-08-07 03:07:08 +0100

    feat: adds inital game manager code

---
+++ assets/js/modules/gamemanager.mjs
@@ -0,0 +1,11 @@
+/**
+ * @module gamemanager
+ */
+
+/**
+ * Game manager. Creates and manages game objects, runs the main game loop
+ * and handles game state and logic.
+ */
+class Game {
+  constructor() {}
+}
