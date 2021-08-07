cb2fa02015ca8c727d7b0d960763d484902782b5
seanyoung247 <seanyoung247@gmail.com>
2021-08-07 13:20:18 +0100

    feat: Adds initial game management creation code

--- assets/js/modules/gamemanager.mjs
+++ assets/js/modules/gamemanager.mjs
@@ -2,10 +2,22 @@
  * @module gamemanager
  */
 
+import { ImgAsset, PixelImg, Renderer, Camera } from "./rendermanager.mjs";
+
 /**
  * Game manager. Creates and manages game objects, runs the main game loop
  * and handles game state and logic.
  */
 class Game {
-  constructor() {}
+  /**
+   * Creates a new game
+   *  @param {Object} canvas The canvas element to draw the game view to
+   */
+  constructor(canvas) {
+    // Create Renderer
+    this._renderer = new Renderer(canvas);
+    
+  }
 }
+
+export { Game };
