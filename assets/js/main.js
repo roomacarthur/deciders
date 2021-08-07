9f00e50e809d78b7eca18b3c726640a57129325b
seanyoung247 <seanyoung247@gmail.com>
2021-08-07 10:39:44 +0100

    refactor: Variable names in test code

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,15 +3,13 @@
 import { PixelImg, Renderer } from "./modules/rendermanager.mjs";
 
 const floorImg = new PixelImg("./assets/img/tracks/test.png", floorLoaded);
-const renderManager = new Renderer(document.getElementById('canvas'));
-renderManager.camera.direction.x = Math.cos(1.57079632679);
-renderManager.camera.direction.y = Math.sin(1.57079632679);
+const renderer = new Renderer(document.getElementById('canvas'));
+renderer.camera.direction.x = Math.cos(1.57079632679);
+renderer.camera.direction.y = Math.sin(1.57079632679);
 
 function floorLoaded() {
-  let ctx = document.getElementById('canvas').getContext('2d');
-  ctx.drawImage(floorImg._surface,0,0);
-
-  renderManager.projectFloor(floorImg);
+  renderer.drawBackdrop();
+  renderer.projectFloor(floorImg);
 }
 
 
