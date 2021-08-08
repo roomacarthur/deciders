05153ef6583d553012d23e8548d23094b63ff7f7
seanyoung247 <seanyoung247@gmail.com>
2021-08-08 15:46:09 +0100

    feat: Adds initial scenery and pickup creation

--- assets/js/modules/gameobjects.mjs
+++ assets/js/modules/gameobjects.mjs
@@ -100,7 +100,7 @@ class Player extends GameObject {
 
       // Update rotation
       const rotation = ((this._rotation) * ((this._speed / this._maxSpeed)/2));
-      this._direction.rotateByRadians(rotation * timeDelta);
+      if(this._speed > 0) this._direction.rotateByRadians(rotation * timeDelta);
     }
 
     // Update position
@@ -158,14 +158,22 @@ class CheckPoint extends GameObject {
  * Base class for an object that the player can pickup. Obstacle or Power up
  */
 class Pickup extends GameObject {
-
+  constructor(game, sprite, position, template, id) {
+    super(game, sprite, position, template);
+    this._active = false;
+    this._id = id;
+  }
 }
 
 /**
  * Defines a peice of map scenery the player can collide with
  */
 class Scenery extends GameObject {
-
+  constructor(game, sprite, position, template, id) {
+    super(game, sprite, position, template);
+    this._active = false;
+    this._id = id;
+  }
 }
 
 /**
