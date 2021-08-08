eeb7bac100e59689daa5c8359a12ca441fd2920e
seanyoung247 <seanyoung247@gmail.com>
2021-08-08 12:37:52 +0100

    feat: Adds game object and checkpoint list to track object

--- assets/js/modules/track.mjs
+++ assets/js/modules/track.mjs
@@ -1,8 +1,12 @@
 
+import {  } "./gameobjects.mjs";
+
 class Track {
-  constructor(image, template) {
+  constructor(game, image, template) {
     this._image = image;
-    // Create Objects and checkpoints?
+    // Create Objects and checkpoints
+    this._checkPoints = new Array();
+
   }
 
   get image() {return this._image;}
