153bddaf84aea0e8fd0d9c1a8aaec649f8707423
hartnetl <laura.hartnett@outlook.com>
2021-08-08 20:25:21 +0000

    Basic timer - not linked with buttons and no styling

---
+++ timer.js
@@ -0,0 +1,42 @@
+const timer = document.getElementById('stopwatch');
+
+var min = 0;
+var sec = 0;
+var stoptime = true;
+
+function startTimer() {
+  if (stoptime == true) {
+        stoptime = false;
+        timerCycle();
+    }
+}
+function stopTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
+
+function timerCycle() {
+    if (stoptime == false) {
+    sec = parseInt(sec);
+    min = parseInt(min);
+
+    sec = sec + 1;
+
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+    }
+
+    if (sec < 10 || sec == 0) {
+      sec = '0' + sec;
+    }
+    if (min < 10 || min == 0) {
+      min = '0' + min;
+    }
+   
+    timer.innerHTML = min + ':' + sec 
+
+    setTimeout("timerCycle()", 1000);
+  }
+}
