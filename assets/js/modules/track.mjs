7f0fedec12e70f5d60a7780ae03fbe8bf17768ac
seanyoung247 <seanyoung247@gmail.com>
2021-08-08 15:47:04 +0100

    feat: Adds sky and ground colour to track

--- assets/js/modules/track.mjs
+++ assets/js/modules/track.mjs
@@ -1,15 +1,28 @@
 
-import { CheckPoint } from "./gameobjects.mjs";
+import { Game, AssetTypes } from "./gamemanager.mjs";
+import { CheckPoint, ObjectFactory } from "./gameobjects.mjs";
 
 class Track {
   constructor(game, image, template) {
+    this._game = game;
     this._image = image;
+    this._template = template;
+    // Load track mask
+    this._mask = game.getAsset(game.addAsset(template.mask, AssetTypes.IMAGE))
+
     // Create Objects and checkpoints
+    this._createCheckpoints(template);
+  }
+
+  _createCheckpoints(template) {
     this._checkPoints = new Array();
 
   }
 
   get image() {return this._image;}
+
+  get skyColor() {return this._template.skyColor;}
+  get groundColor() {return this._template.groundColor;}
 }
 
 export { Track };
